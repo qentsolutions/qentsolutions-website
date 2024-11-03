b5b66ef1f7c30b0c7fa1956aacb31f8023ad5c80
qentsolutions <contact@qentsolutions.com>
2024-11-03 23:54:32 +0100

    add all

--- app/(marketing)/(routes)/contact/page.tsx
+++ app/(marketing)/(routes)/contact/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { Field, Label, Switch } from "@headlessui/react";
 import { toast } from "react-hot-toast";
+import { Mail, MessageSquare, Phone } from "lucide-react";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -97,165 +98,197 @@ export default function Contact() {
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact <span className="text-blue-600"> sales</span>
-        </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Ready to take the next step? Fill out the form below and we&apos;ll
-          <br /> get back to you as soon as possible.
-        </p>
-      </div>
-      <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
-            <label
-              htmlFor="firstName"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="firstName"
-                id="first-name"
-                autoComplete="given-name"
-                onChange={handleChange}
-                required
-                value={formData.firstName}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+      <div className="flex justify-around">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Contact <span className="text-blue-600"> sales</span>
+          </h2>
+          <div className="mt-10 space-y-8">
+            <div className="flex items-center gap-x-4">
+              <Mail className="h-6 w-6 text-blue-600" />
+              <div>
+                <p className="font-semibold text-gray-900">Email</p>
+                <a href="mailto:info@snappy.io" className="text-gray-600 hover:text-blue-600">
+                  contact@qentsolutions.com
+                </a>
+              </div>
             </div>
-          </div>
-          <div>
-            <label
-              htmlFor="lastName"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="lastName"
-                id="last-name"
-                autoComplete="family-name"
-                onChange={handleChange}
-                required
-                value={formData.lastName}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="company"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Company (optional)
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                value={formData.company}
-                autoComplete="organization"
-                onChange={handleChange}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                onChange={handleChange}
-                required
-                value={formData.email}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+
+      
+
+            <div className="flex items-center gap-x-4">
+              <MessageSquare className="h-6 w-6 text-blue-600" />
+              <div>
+                <p className="font-semibold text-gray-900">Customer Support</p>
+                <p className="text-gray-600">
+                  Our support team is available around the clock <br /> to address any questions or updates you may have.
+                </p>
+              </div>
             </div>
           </div>
 
-          <div>
-            <input
-              type="text"
-              name="honeypot"
-              id="honeypot"
-              className="hidden"
-              onChange={handleChange}
-              value={formData.honeypot}
-            />
+        </div>
+        <div>
+          <div className="mx-auto max-w-2xl text-center">
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              Ready to take the next step? Fill out the form below and we&apos;ll
+              <br /> get back to you as soon as possible.
+            </p>
           </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                onChange={handleChange}
-                required
-                value={formData.message}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <Field as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                aria-required="true"
-                className={classNames(
-                  agreed ? "bg-indigo-600" : "bg-gray-200",
-                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? "translate-x-3.5" : "translate-x-0",
-                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out",
-                  )}
+          <form onSubmit={handleSubmit} className="mx-auto mt-12 max-w-xl sm:mt-8">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="first-name"
+                    autoComplete="given-name"
+                    onChange={handleChange}
+                    required
+                    value={formData.firstName}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="last-name"
+                    autoComplete="family-name"
+                    onChange={handleChange}
+                    required
+                    value={formData.lastName}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="company"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Company (optional)
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    value={formData.company}
+                    autoComplete="organization"
+                    onChange={handleChange}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    onChange={handleChange}
+                    required
+                    value={formData.email}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  name="honeypot"
+                  id="honeypot"
+                  className="hidden"
+                  onChange={handleChange}
+                  value={formData.honeypot}
                 />
-              </Switch>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    onChange={handleChange}
+                    required
+                    value={formData.message}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <Field as="div" className="flex gap-x-4 sm:col-span-2">
+                <div className="flex h-6 items-center">
+                  <Switch
+                    checked={agreed}
+                    onChange={setAgreed}
+                    aria-required="true"
+                    className={classNames(
+                      agreed ? "bg-indigo-600" : "bg-gray-200",
+                      "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+                    )}
+                  >
+                    <span className="sr-only">Agree to policies</span>
+                    <span
+                      aria-hidden="true"
+                      className={classNames(
+                        agreed ? "translate-x-3.5" : "translate-x-0",
+                        "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out",
+                      )}
+                    />
+                  </Switch>
+                </div>
+                <Label className="text-sm leading-6 text-gray-600">
+                  By selecting this, you agree to our{" "}
+                  <a href="#" className="font-semibold text-indigo-600">
+                    privacy policy
+                  </a>
+                  .
+                </Label>
+              </Field>
             </div>
-            <Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy policy
-              </a>
-              .
-            </Label>
-          </Field>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              >
+                Let&apos;s talk
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-          >
-            Let&apos;s talk
-          </button>
-        </div>
-      </form>
+      </div>
+
+
     </div>
   );
 }
--- app/(marketing)/(routes)/pricing/page.tsx
+++ app/(marketing)/(routes)/pricing/page.tsx
@@ -603,7 +603,7 @@ const Pricing = () => {
 
         <FAQ />
         <div className="mx-auto max-w-2xl py-20 sm:px-6 sm:pt-14 sm:pb-24 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gradient-to-r from-blue-400 to-indigo-600 px-6 pt-16 pb-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="relative isolate overflow-hidden bg-gradient-to-r from-blue-400 to-indigo-600 px-6 pt-16 pb-14 md:pb-8 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-14 lg:text-left">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
                 Got a question ?
